a66afed22dfa82768d946f64636e9138bc7e7aaa
André Potthast <andre.potthast.ap@googlemail.com>
2024-01-04 21:09:59 +0100

    Js done without error handling and Usermessage for not found location

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ const userInputForm = document.querySelector("form");
 userInputForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = userInputForm.querySelector("input").value;
-  getWeatherData(location).then((data) => {
-    console.log(data);
-  });
+  getWeatherData(location).then((data) => {});
 });
 
 // weather api
-const apiKey = "f5dbff6b0de347cd86d101159232712";
 async function getWeatherData(location) {
+  const apiKey = "f5dbff6b0de347cd86d101159232712";
   try {
     //3 days forecast don't give me an error even for current so should be fine
     let url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3`;
@@ -22,7 +20,9 @@ async function getWeatherData(location) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
-    return processWeatherData(data);
+    const processedData = processWeatherData(data);
+    displayWeather(processedData);
+    return processedData;
   } catch (error) {
     console.error("Fetch failed:", error);
   }
@@ -30,7 +30,7 @@ async function getWeatherData(location) {
 
 function processWeatherData(rawData) {
   let currentData = {
-    [`today`]: {
+    [`now`]: {
       location: rawData.location.name,
       localionRegion: rawData.location.region,
       date: rawData.forecast.forecastday[0].date,
@@ -53,3 +53,49 @@ function processWeatherData(rawData) {
 
   return currentData;
 }
+
+function displayWeather(data) {
+  console.log(data);
+
+  const now = document.querySelector(".now");
+  now.querySelector(".location").textContent = data.now.location;
+  now.querySelector("img").src = data.now.conditionIcon;
+  now.querySelector(".temp").textContent = data.now.temp;
+  now.querySelector(".condition").textContent = data.now.condition;
+  now.querySelector(".wind").textContent = data.now.wind;
+  now.querySelector(".humidity").textContent = data.now.humidity;
+
+  const forecast = document.querySelectorAll(".forecast");
+  forecast.forEach((forecast) => {
+    let forecastDay;
+    if (forecast.classList.contains("today")) {
+      forecastDay = 0;
+    } else if (forecast.classList.contains("tomorrow")) {
+      forecastDay = 1;
+    } else if (forecast.classList.contains("twoDays")) {
+      forecastDay = 2;
+    }
+    forecast.querySelector(".date").textContent = formatDateShort(
+      data[forecastDay].date
+    );
+    forecast.querySelector("img").src = data[forecastDay].conditionIcon;
+    forecast.querySelector(".condition").textContent =
+      data[forecastDay].condition;
+    forecast.querySelector(".min-temp").textContent = data[forecastDay].minTemp;
+    forecast.querySelector(".max-temp").textContent = data[forecastDay].maxTemp;
+  });
+}
+
+window.addEventListener("load", () => {
+  getWeatherData("Berlin");
+});
+
+function formatDateShort(inputDate) {
+  const date = new Date(inputDate);
+  const day = date.getDate();
+  const month = date.getMonth() + 1; //
+  const formattedDay = day < 10 ? "0" + day : day;
+  const formattedMonth = month < 10 ? "0" + month : month;
+  return formattedDay + "/" + formattedMonth;
+}
+// todo error handling
